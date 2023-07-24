3896dd55f33829dddf03ba2ef1634b50f78dbb2c
mollysullivan6 <mollyksullivan6@gmail.com>
2023-07-24 12:03:22 -0400

    created a function

--- DroneApp3/test_mqtt.py
+++ DroneApp3/test_mqtt.py
@@ -36,13 +36,37 @@ while not connected:
     pass
 
 
+# # old
+# task_array = ["Set geofence parameters to altitude of 300.", "Raise throttle once you see the drone takeoff." , "Change RTL parameters to a different home location." , "Change to RTL to test the change.", "Change mode from stabilized to land." ]
+# for task in task_array:
+#     print('publishing')
+#     task_json = json.dumps(task)
+#     client.publish(topic, task_json.strip('\"'))
+#     time.sleep(10)
 
-task_array = ["Set geofence parameters to altitude of 300.", "Raise throttle once you see the drone takeoff." , "Change RTL parameters to a different home location." , "Change to RTL to test the change.", "Change mode from stabilized to land." ]
 
-for task in task_array:
-    print('publishing')
-    task_json = json.dumps(task)
-    client.publish(topic, task_json.strip('\"'))
-    time.sleep(10)
+
+# function that publishes a series of tasks 
+def message(tasks_arr, time_arr):
+    for i in range(len(tasks_arr)):
+        time.sleep(time_arr[i])
+        print('publishing')
+        task_json = json.dumps(tasks_arr[i])
+        client.publish(topic, task_json.strip('\"'))
+        
+# test 1 
+test1_tasks = ["Place the throttle in the central position", "Switch to stabilized mode", "Change mode to RTL"]
+test1_time = [25, 90, 60]
+message(test1_tasks, test1_time)
+
+# test 2
+test2_tasks = ["Place the throttle just above the neutral position", "Deactivate the geofence", "Change mode to POS HOLD", "Change mode to STABILIZE"]
+test2_time = [30, 45, 60, 5]
+message(test2_tasks, test2_time)
+
+# test 3
+test3_tasks = ["Hit kill switch"]
+test3_time = [50]
+message(test3_tasks, test3_time)
 
 
